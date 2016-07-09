52d85e17d8482dcfd0e943404e744a6d2b39ff71
José Medeiros <jmedeiros@agap2.pt>
2016-07-09 18:51:26 +0100

    Added sample projects for day one

---
+++ Day 1/Workshop/Workshop.iOS/DetailViewController.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace Workshop.iOS
+{
+    public partial class DetailViewController : UIViewController
+    {
+        public DetailViewController (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
---
+++ Day 1/Workshop/Workshop.iOS/ViewController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+using UIKit;
+
+namespace Workshop.iOS
+{
+    public partial class ViewController : UIViewController
+    {
+        protected ViewController(IntPtr handle) : base(handle)
+        {
+
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+            messageButton.TouchUpInside += MessageButton_TouchUpInside;
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+
+        void MessageButton_TouchUpInside(object sender, EventArgs e)
+        {
+            messageLabel.Text = "Goodbye world";
+        }
+    }
+}
+
