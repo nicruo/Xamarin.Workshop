cba9cfc9e15d90e210de71136572d9c12ccdce49
José Medeiros <jmedeiros@agap2.pt>
2016-07-12 18:30:03 +0100

    Added Day 1 Forms sample

---
+++ Day 1/WorkshopForms/WorkshopForms/WorkshopFormsPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Xamarin.Forms;
+using System.Net.Http;
+
+namespace WorkshopForms
+{
+    public partial class WorkshopFormsPage : ContentPage
+    {
+        public WorkshopFormsPage()
+        {
+            InitializeComponent();
+        }
+
+        async void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            var client = new HttpClient();
+
+            var response = await client.GetAsync("http://www.mocky.io/v2/578527af0f0000620bc28956");
+
+            entry.Text = await response.Content.ReadAsStringAsync();
+        }
+    }
+}
+
